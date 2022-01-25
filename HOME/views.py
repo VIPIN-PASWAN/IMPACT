aea09716dfaebd532928ed4f85dce8966e47841e
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2022-01-26 01:10:27 +0530

    Contact Us page designed

--- HOME/views.py
+++ HOME/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from HOME.models import Contact
 
 # Create your views here.
+
+
 
 def home(request):
     return render(request, 'home/home.html')
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        contact = Contact(name=name, email=email, phone=phone, content=content)
+        contact.save()
     return render(request, 'home/contact.html')
 
 
