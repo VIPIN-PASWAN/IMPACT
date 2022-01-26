4b8bbb4a76d3b7a4908dd2863a3709144d6c1bff
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2022-01-27 02:26:14 +0530

    home page designed i.e., nav bar, date time and cards

--- static/script.js
+++ static/script.js
@@ -8,3 +8,10 @@ messages.style.display = "none";
 function CloseMsgBox() {
 messages.style.display = "none";
 }
+
+var today = new Date();
+ var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+ const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ const d = new Date();
+ let day = weekday[d.getDay()];
+ document.getElementById("currentDate").innerHTML = date.concat(",", day) ;
