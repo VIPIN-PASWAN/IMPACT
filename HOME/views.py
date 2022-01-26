42769de6ae90c1168714577254582e94041f3894
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2022-01-26 23:22:19 +0530

    added alert message function to contact page using javascript

--- HOME/views.py
+++ HOME/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.http import HttpResponse
 from HOME.models import Contact
@@ -16,8 +17,14 @@ def contact(request):
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
-        contact = Contact(name=name, email=email, phone=phone, content=content)
-        contact.save()
+
+        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 4:
+            messages.error(request, "Please fill the form correctly !")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+
     return render(request, 'home/contact.html')
 
 
---
+++ static/script.js
@@ -0,0 +1,10 @@
+//Close function for messages
+var messages = document.getElementById("messages_container");
+
+setTimeout(function(){
+messages.style.display = "none";
+},3000);
+
+function CloseMsgBox() {
+messages.style.display = "none";
+}
