802c66c672ae95f65342aa7025275a4ca2766a76
VIPIN-PASWAN <vipin1911016@akgec.ac.in>
2022-02-05 01:16:43 +0530

    Added TinyMCE for easy writting of blog. Code opimisation in js file for good preview of blog at blogHome page.

--- BLOG/admin.py
+++ BLOG/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from BLOG.models import Post, BlogComment
+
 # Register your models here.
 
-admin.site.register((Post, BlogComment))
+admin.site.register(BlogComment)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('injectTinyMCE.js',)
--- static/script.js
+++ static/script.js
@@ -5,9 +5,14 @@ setTimeout(function(){
 $('.alert').alert('close');
 },3000);
 
-var today = new Date();
- var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
- const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let today = new Date();
+let date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+const weekday = ["Sun","Mon","Tue","Wed","Thur","Fri","Sat"];
  const d = new Date();
  let day = weekday[d.getDay()];
  document.getElementById("currentDate").innerHTML = date.concat(",", day) ;
+
+ let previews=document.getElementsByClassName('preview');
+      Array.from(previews).forEach((element)=>{
+          element.innerHTML=element.innerText;
+      })
